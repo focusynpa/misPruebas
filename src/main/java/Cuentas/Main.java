8d1bddf8e4816af48face48082a6298fd5a2f7c6
Darias_Hughes_Patricia <ProBook@DESKTOP-JLG310C>
2022-06-08 21:02:24 +0100

    Tarea04 Refactorización

--- src/main/java/Cuentas/Main.java
+++ src/main/java/Cuentas/Main.java
@@ -5,7 +5,10 @@ public class Main {
     public static void main(String[] args) {
         operativa_cuenta(0);
     }
-
+/**
+ * 
+ * @param cantidad 
+ */
     private static void operativa_cuenta(float cantidad) {
         CCuenta cuenta1;
         double saldoActual;
